4f695c8c3a76ad1508ae6d43daee65bbd8f87443
Domenico <mazzinidomenico@gmail.com>
2023-07-02 19:53:43 +0200

    Modifiche varie da Michele

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import './style/App.css'
 import {Routes, Route, Navigate} from 'react-router-dom'
 
 import Authentication from "./pages/Authentication"
-import HomePage from "./pages/HomePage"
 import ChatPage from "./pages/ChatPage"
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<HomePage/>}/>
                 <Route path="/chat" element={<ChatPage/>}/>
                 <Route path="/login" element={<Authentication login={true}/>}/>
                 <Route path="/signup" element={<Authentication login={false}/>}/>
-                <Route path="*" element={<Navigate to='/' replace />} />
+                <Route path="*" element={<Navigate to='/login' replace />} />
             </Routes>
         </>
     );
